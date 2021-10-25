c4a7193096776cf4f8a463f679ac5f6161b1ce85
La Huy <laquanghuy231100@gmail.com>
2021-10-26 01:13:22 +0700

    first commit

---
+++ src/pages/Catalog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Helmet from '../components/Helmet'
+
+const Catalog = () => {
+    return (
+        <Helmet title="Sản phẩm">
+            chua lam 
+        </Helmet>
+    )
+}
+
+export default Catalog
---
+++ src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+    return (
+        <div>
+            thieu page nay
+        </div>
+    )
+}
+
+export default Product
